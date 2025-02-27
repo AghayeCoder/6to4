44fb2d3d14265fcbe743c6bc54907fe82d636be0
Aghaye Coder <ahmand@vk.com>
2025-02-27 19:46:31 +0330

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import time
+
+receiver = sys.argv[1]
+print("Remote Ro -> {}".format(receiver))
+os.system("ip tunnel add tun6to4 mode sit ttl 254 remote {}".format(receiver))
+os.system("ip link set dev tun6to4 up")
+os.system("ip addr add fc01::1/64 dev tun6to4")
+time.sleep(3)
+os.system("ip tunnel add gre1 mode ip6gre remote fc01::2 local fc01::1")
+os.system("ip link set gre1 up")
+os.system("ip addr add 10.10.15.1/30 dev gre1")
+time.sleep(3)
+os.system("ip route add default via 10.10.15.2 table 4")
+os.system("cat /dev/null > ~/.bash_history")
+os.system("rm -rf sender.py")
