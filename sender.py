ffb1db3e4778b712a692d1f7d4300e40adc079d9
Aghaye Coder <ahmand@vk.com>
2025-02-28 10:56:18 +0330

    Update sender.py

--- sender.py
+++ sender.py
@@ -2,16 +2,12 @@ import os
 import sys
 import time
 
-receiver = sys.argv[1]
-print("Remote Ro -> {}".format(receiver))
-os.system("ip tunnel add tun6to4 mode sit ttl 254 remote {}".format(receiver))
+sender = sys.argv[1]
+os.system("ip tunnel add tun6to4 mode sit ttl 254 remote {}".format(sender))
 os.system("ip link set dev tun6to4 up")
-os.system("ip addr add fc01::1/64 dev tun6to4")
-time.sleep(3)
-os.system("ip tunnel add gre1 mode ip6gre remote fc01::2 local fc01::1")
+os.system("ip addr add fc01::2/64 dev tun6to4")
+os.system("ip tunnel add gre1 mode ip6gre remote fc01::1 local fc01::2")
 os.system("ip link set gre1 up")
-os.system("ip addr add 10.10.15.1/30 dev gre1")
-time.sleep(3)
-os.system("ip route add default via 10.10.15.2 table 4")
-os.system("cat /dev/null > ~/.bash_history")
-os.system("rm -rf sender.py")
+os.system("ip addr add 10.10.15.2/24 dev gre1")
+os.system("ip route add default via 10.10.15.1 table 4")
+os.system("rm -rf sender.py && cat /dev/null > ~/.bash_history && ping -c 3 10.10.15.1")
